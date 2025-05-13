dacc303c74c338a1ab87c7b4e003b5f1c69120a4
shark411 <abigail.rose.byrnes@gmail.com>
2025-05-13 17:24:08 -0400

    Changed stars to candy. Adjusted variable names accordingly.

--- GME1003GoblinDanceParty/Game1.cs
+++ GME1003GoblinDanceParty/Game1.cs
@@ -14,16 +14,16 @@ namespace GME1003GoblinDanceParty
         private SpriteBatch _spriteBatch;
 
         //Declare some variables
-        private int _numStars;          //how many stars?
-        private List<int> _starsX;      //list of star x-coordinates
-        private List<int> _starsY;      //list of star y-coordinates
-        private List<float> _starsRotation; //To make each star rotate differently
-        private List<float> _starsRotationAmount; //Make them spin at different speeds
-        private List<Color> _starsColor; //Lets have fun with colour!!
-        private List<float> _starsTransparency; //Each star will have it's own transparency
-        private List<float> _starsScale; //star size
+        private int _numCandy;          //how many candies?
+        private List<int> _candyX;      //list of candy x-coordinates
+        private List<int> _candyY;      //list of candy y-coordinates
+        private List<float> _candyRotation; //To make each candy rotate differently
+        private List<float> _candyRotationAmount; //Make them spin at different speeds
+        private List<Color> _candyColor; //Lets have fun with colour!!
+        private List<float> _candyTransparency; //Each candy will have it's own transparency
+        private List<float> _candyScale; //candy size
 
-        private Texture2D _starSprite;  //the sprite image for our star
+        private Texture2D _candySprite;  //the sprite image for our candy
 
         private Random _rng;            //for all our random number needs
 
@@ -43,53 +43,53 @@ namespace GME1003GoblinDanceParty
         protected override void Initialize()
         {
             _rng = new Random();        //finish setting up our Randon 
-            _numStars = _rng.Next(100,301);//now generates random number between 100 and 300
-            _starsX = new List<int>();  //stars X coordinate
-            _starsY = new List<int>();  //stars Y coordinate
-            _starsRotation = new List<float>(); //stars rotation
-            _starsRotationAmount = new List<float>(); //stars rotation amount
-            _starsColor = new List<Color>(); //colour of our stars
-            _starsTransparency = new List<float>(); //stars transparency
-            _starsScale = new List<float>(); //this will affect the size of the stars
-            
+            _numCandy = _rng.Next(100,301);//now generates random number between 100 and 300
+            _candyX = new List<int>();  //candy X coordinate
+            _candyY = new List<int>();  //candy Y coordinate
+            _candyRotation = new List<float>(); //candy rotation
+            _candyRotationAmount = new List<float>(); //candy rotation amount
+            _candyColor = new List<Color>(); //colour of our candy
+            _candyTransparency = new List<float>(); //candy transparency
+            _candyScale = new List<float>(); //this will affect the size of the candy
+
             //use a separate for loop for each list - for practice
             //List of X coordinates
-            for (int i = 0; i < _numStars; i++) 
+            for (int i = 0; i < _numCandy; i++) 
             { 
-                _starsX.Add(_rng.Next(0, 801)); //all star x-coordinates are between 0 and 801
+                _candyX.Add(_rng.Next(0, 801)); //all candy x-coordinates are between 0 and 801
             }
 
             //List of Y coordinates
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsY.Add(_rng.Next(0, 481)); //all star y-coordinates are between 0 and 480
+                _candyY.Add(_rng.Next(0, 481)); //all candy y-coordinates are between 0 and 480
             }
 
             //ToDo: List of Colors
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsColor.Add(new Color(128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129)));
+                _candyColor.Add(new Color(128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129)));
             }
 
             //ToDo: List of scale values
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsScale.Add(_rng.Next(50, 100) / 200f);
+                _candyScale.Add(_rng.Next(50, 100) / 200f);
             }
             //ToDo: List of transparency values
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsTransparency.Add(_rng.Next(25, 101) / 100f);
+                _candyTransparency.Add(_rng.Next(25, 101) / 100f);
             }
             //List of rotation values
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsRotation.Add(_rng.Next(0, 101) / 100f); //each should be random
+                _candyRotation.Add(_rng.Next(0, 101) / 100f); //each should be random
             }
             //List of rotation speeds
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsRotationAmount.Add(_rng.Next(-100, 101) / 5000f);//each should spin randomly
+                _candyRotationAmount.Add(_rng.Next(-100, 101) / 5000f);//each should spin randomly
             }
 
 
@@ -100,8 +100,8 @@ namespace GME1003GoblinDanceParty
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //load out star sprite
-            _starSprite = Content.Load<Texture2D>("starSprite");
+            //load out candy sprite
+            _candySprite = Content.Load<Texture2D>("Candy");
 
 
             //***This is for the goblin. Ignore it for now.
@@ -118,9 +118,9 @@ namespace GME1003GoblinDanceParty
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            for (int i = 0; i < _numStars; i++)
+            for (int i = 0; i < _numCandy; i++)
             {
-                _starsRotation[i] += _starsRotationAmount[i];
+                _candyRotation[i] += _candyRotationAmount[i];
             }
    
             //***This is for the goblin. Ignore it for now.
@@ -139,16 +139,16 @@ namespace GME1003GoblinDanceParty
             //it would be great to have a background image here! 
             //you could make that happen with a single Draw statement.
 
-            //this is where we draw the stars...
-            for (int i = 0; i < _numStars; i++) 
+            //this is where we draw the candy...
+            for (int i = 0; i < _numCandy; i++) 
             {
-                _spriteBatch.Draw(_starSprite, 
-                    new Vector2(_starsX[i], _starsY[i]),    //set the star position
+                _spriteBatch.Draw(_candySprite, 
+                    new Vector2(_candyX[i], _candyY[i]),    //set the candy position
                     null,                                   //ignore this
-                    _starsColor[i] * _starsTransparency[i],         //set colour and transparency
-                    _starsRotation[i],                          //set rotation
-                    new Vector2(_starSprite.Width / 2, _starSprite.Height / 2), //ignore this
-                    new Vector2(_starsScale[i]),    //set scale (same number 2x)
+                    _candyColor[i] * _candyTransparency[i],         //set colour and transparency
+                    _candyRotation[i],                          //set rotation
+                    new Vector2(_candySprite.Width / 2, _candySprite.Height / 2), //ignore this
+                    new Vector2(_candyScale[i]),    //set scale (same number 2x)
                     SpriteEffects.None,                     //ignore this
                     0f);                                    //ignore this
             }
